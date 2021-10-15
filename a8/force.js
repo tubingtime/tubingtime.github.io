a68c2d1b40b37c153ce407f185aab2b46ecfa1cc
tubingtime <tdelaveaga@dons.usfca.edu>
2021-10-15 13:35:19 -0700

    g2g food pantry

--- a8/force.js
+++ a8/force.js
@@ -9,12 +9,15 @@ d3.csv("soc-firm-hi-tech.csv", function(d, i) {
   let src = d["source"];
   let dest = d["destination"];
 
-  if (nodeArr[src] == null){
+  /* Not sure how I would go about checking for duplicates using this javascript anon function stuff 
+      I would need to access the data from the d array but idk how */
+
+  if (nodeArr[src] == null){ 
     nodeArr[src] = {
       id : src
     }
   }
-  if (nodeArr[dest] == null){
+  if (nodeArr[dest] == null){ 
     nodeArr[dest] = {
       id : dest
     }
@@ -25,7 +28,7 @@ d3.csv("soc-firm-hi-tech.csv", function(d, i) {
     destination : dest
   }
 
-}).then(function(d) {
+}).then(function() {
   
   console.log(nodeArr);
   console.log(linkArr);
@@ -37,12 +40,48 @@ d3.csv("soc-firm-hi-tech.csv", function(d, i) {
   const innerHeight = h - margin.top - margin.bottom;
 
 
+
   let svg = d3.select("svg")
           .attr("width", w)
           .attr("height", h);
-  const g = svg.append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`)
+/*   const g = svg.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`) */
+  
+  /* Setup physics engine */
+  let forceSim = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function(node){
+      return node.id /* hmmm i dont think this will work */
+    }))
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(w/2, h/2));
+  
+  let links = svg.append("g")
+    .attr("class","links")
+    .selectAll("line")
+    .data(linkArr)
+    .enter()
+    .append("line");
+    links.attr("stroke-width", 1);
+  
+  let nodes = svg.append("g")
+    .attr("class","nodes")
+    .selectAll("circle")
+    .data(nodeArr)
+    .enter()
+    .append("circle");
+  nodes
+    .attr("r",5)
+    .attr("fill", "black");
 
+  nodes.append("title")
+    .text(function(d){return d.id});
+  
+  /* Add nodes */
+  forceSimulation
+    .nodes(nodeArr)
+    .on("tick",ticked);
+  /* Add links */
+  
   
     
 });
