78d6779561d06c3e1477a146f255015fca304b59
thomas <tdelaveaga@dons.usfca.edu>
2025-05-14 16:40:03 -0700

    update bg, blurb, start svelte redesign

---
+++ myapp/src/lib/footer.svelte
@@ -0,0 +1,26 @@
+<script>
+    let { active } = $props();
+    let home = "inactive";
+    let services = "inactive"
+    let about = "inactive"
+
+    if (active == "home") {
+        home = "active"
+    }
+    else if (active == "services") {
+        services = "active"
+    }
+    else if (active == "about") {
+        about ="active"
+    }
+    else {
+        console.log("Invalid navbar item!")
+    }
+
+</script> 
+
+<div id="nav">
+	<a id={home} href="home">Home</a>
+	<a id={services} href="services">Services</a>
+	<a id={about} href="about">About</a>
+</div>
---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,41 @@
+	<script>
+		import Nav from "$lib/nav.svelte";
+		import Footer from "$lib/footer.svelte";
+	</script>
+
+	<div id="bg"></div>
+	<div id="main">
+		<Nav active={"home"} />
+		<!-- Header -->
+		<header id="header">
+			<h1>Thomas de Laveaga</h1>
+			<p>Hi! My name is Thomas. <br>
+				<br>
+				I'm a freelance computer consultant.
+				<br>Use the links below to discover the services I offer.
+			</p>
+			<section style="margin: 1em;">
+				<button type="button" class="nes-btn">Contact me</button>
+
+			</section>
+
+
+		</header>
+
+
+
+		<!-- Footer -->
+		<footer id="footer">
+			<div style="display:flex; flex-direction: column;">
+				<div>
+					<a href="mai&#108;to&#58;tj&#x40;th&#37;6F&#109;%61%73de&#37;6C&#97;&#118;&#101;&#37;61ga%2E%63&#111;m" class="nes-icon gmail is-medium"></a>
+					<a href="https://www.linkedin.com/in/thomas-de-laveaga-18a0851b0/" class="nes-icon linkedin is-medium"></a>
+					<a href=" https://github.com/tubingtime/" class="nes-icon github is-medium"></a>
+				</div>
+				<div>
+					<span class="copyright">&copy; Thomas de Laveaga. </span>
+				</div>
+			</div>
+		</footer>
+
+	</div>
