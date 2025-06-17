dd19aa68ff2fd8378ebfc55ef890065c4b2edd7e
TJ de Laveaga <60046785+tubingtime@users.noreply.github.com>
2025-06-17 16:20:56 -0700

    update icon + remove typo

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -29,7 +29,7 @@
 			</div>
 			<div class="service">
 				<div class="flex-row service-header">
-					<Icon icon="streamline-pixel:internet-network-wifi-monitor" width="3em" height="3em" class="icon" />
+					<Icon icon="streamline-pixel:internet-network-www" width="3em" height="3em" class="icon" />
 					<h2>Web Design & Hosting</h2>
 				</div>
 				<p>Want a website like this or something more advanced? <br>
@@ -39,7 +39,7 @@
 		<div class="center-row">
 			<div class="service">
 				<div class="flex-row service-header">
-					<Icon icon="streamline-pixel:internet-network-www" width="3em" height="3em" class="icon" />
+					<Icon icon="streamline-pixel:internet-network-wifi-monitor" width="3em" height="3em" class="icon" />
 					<h2>Remote IT Support</h2>
 				</div>
 				<p>Remote IT support done in a responsive and understanding way. </p>
@@ -49,7 +49,7 @@
 					<Icon icon="streamline-pixel:coding-apps-websites-setting-computer" width="3em" height="3em" class="icon" />
 					<h2>Home IT Support</h2>
 				</div>
-				<p>Need help with home computers, computer hardware, printers, TVs, or routers?.</p>
+				<p>Need help with home computers, computer hardware, printers, TVs, or routers?</p>
 			</div>
 	</div>
 
